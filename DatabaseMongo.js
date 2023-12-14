1b2cccc84a286207ed940b10261ac775498d6c31
Caolán Maguire <36414229+calsickofthis@users.noreply.github.com>
2023-12-14 19:29:11 +0000

    mongoDb reading database

--- DatabaseMongo.js
+++ DatabaseMongo.js
@@ -1,49 +1,26 @@
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient
+var coll
 
-var db;
-var coll;
+MongoClient.connect('mongodb://127.0.0.1:27017')
+    .then((client) => {
+        db = client.db('proj2023MongoDB')
+        coll = db.collection('managers')
+    })
+    .catch((error) => {
+        console.log(error.messge)
+    })
 
-async function run() {
-    const uri = "mongodb://127.0.0.1:27017";
-    const client = new MongoClient(uri);
-    try {
-        const database = client.db('proj2023MongoDB');
-        const movies = database.collection('managers');
-        
-    } finally {
-        // Ensures that the client will close when you finish/error
-        await client.close();
-    }
+var findAll = function () {
+    return new Promise((resolve, reject) => {
+        var cursor = coll.find()
+        cursor.toArray()
+            .then((documents) => {
+                resolve(documents)
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
 }
 
-connection = run();
-
-module.exports = {
-    getemployeesMongo: function () {
-        run();
-        var coll;
-        return new Promise((resolve, reject) => {
-            cursor = coll.find()
-            cursor.toArray()
-                .then((data) => {
-                    resolve(data)
-                })
-                .catch((error) => {
-                    reject(error)
-                })
-        })
-    },
-
-    getemployeesMongoAdd: function () {
-        return new Promise((resolve, reject) => {
-            coll.insertOne(employee)
-                .then((documents) => {
-                    resolve(documents)
-                })
-                .catch((error) => {
-                    reject(error)
-                })
-        })
-    }
-
-}
+module.exports = { findAll }
